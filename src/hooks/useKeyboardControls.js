4a8c22f66fd902d4590c2ca4a1b02d2701098762
Brandon Bagby <brandonbagby@Brandons-MacBook-Pro.local>
2021-12-09 05:43:49 -0500

    grass biome complete

--- src/hooks/useKeyboardControls.js
+++ src/hooks/useKeyboardControls.js
@@ -14,10 +14,10 @@ function actionByKey(key){
 
 function textureByKey(key) {
     const keys = {
-        Digit1: 'dirt',
-        Digit2: 'grass',
-        Digit3: 'glass',
-        Digit4: 'wood',
+        Digit1: 'wood',
+        Digit2: 'dirt',
+        Digit3: 'grass',
+        Digit4: 'glass',
         Digit5: 'log'
     };
     return keys[key];
--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,15 +1,16 @@
 import create from 'zustand';
 
 const getLocalStorage = (key) => JSON.parse(window.localStorage.getItem(key));
+localStorage.clear();
 const setLocalStorage = (key, value) =>
 window.localStorage.setItem(key, JSON.stringify(value));
 
 export const useStore = create ((set) => ({
-    cubes: getLocalStorage('world') || [{pos:[0, 0, 0], texture: 'wood'},
-    {pos:[-3, 1, 0], texture: 'dirt'},
-    {pos:[-1, 1, 0], texture: 'grass'},
-    {pos:[0, 2, 0], texture: 'glass'},
-    {pos:[1, 1, 0], texture: 'log'}],
+    cubes: getLocalStorage('world') || [{pos:[1, 1, 0], texture: 'wood'},
+    {pos:[2, 1, 0], texture: 'dirt'},
+    {pos:[3, 1, 0], texture: 'grass'},
+    {pos:[4, 1, 0], texture: 'glass'},
+    {pos:[5, 1, 0], texture: 'log'}],
     addCube: (x, y, z, texture) => 
     set(state => ({
         cubes: [...state.cubes, { pos: [x, y, z], texture }],
